772468f1c5e59ae65b9e53c1fbe7fedfdd9b03ad
aungwanakyimal <59996144+aungwanakyimal@users.noreply.github.com>
2021-08-04 22:06:51 +0630

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-   $('.generate-iframe').click(function(){
+$('.generate-iframe').click(function(){
 
 	
 
@@ -8,13 +8,35 @@ $( document ).ready(function() {
                 var url = $('#iframe-url').val();
 
 
+    
+$('#iframe-output-area1').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'.mp4');
+$('#iframe-output-area2').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_hd.mp4');
+$('#iframe-output-area3').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_dvd.mp4');
+$('#iframe-output-area4').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_std.mp4');
 
 
-		$('#iframe-output-area1').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'.mp4');
-                $('#iframe-output-area2').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_hd.mp4');
-                $('#iframe-output-area3').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_dvd.mp4');
-                $('#iframe-output-area4').val('https://videos.files.wordpress.com/'+ code +'/'+ url +'_mp4_std.mp4');
+
+
 	});
 });
 
-// https://videos.files.wordpress.com/sctAQyVA/crouchingtigerhiddendragon-swordofdestiny5b20165daungwana.mp4 //
+function copy1() {
+  let textarea = document.getElementById("iframe-output-area1");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy2() {
+  let textarea = document.getElementById("iframe-output-area2");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy3() {
+  let textarea = document.getElementById("iframe-output-area3");
+  textarea.select();
+  document.execCommand("copy");
+}
+function copy4() {
+  let textarea = document.getElementById("iframe-output-area4");
+  textarea.select();
+  document.execCommand("copy");
+}
